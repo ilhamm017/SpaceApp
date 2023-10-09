472c9d543163083a10aee6db2b6e198715476d7e
ilhamm017 <ilhammuliawan26@gmail.com>
2023-10-09 23:48:56 +0700

    menghapus yang tidak perlu dan memperbaiki bug

--- app/src/main/java/com/example/spaceapp/MainActivity.kt
+++ app/src/main/java/com/example/spaceapp/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.spaceapp
 
-import android.graphics.Paint.Style
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -30,9 +27,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -42,7 +36,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.spaceapp.ui.theme.SpaceAppTheme
-import kotlin.math.absoluteValue
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -83,7 +77,6 @@ fun SpaceApp() {
                     .padding(20.dp)
             ) {
                 Surface {
-                    Text(text = "Konten"+currentIndex)
                     when (currentIndex) {
                         1 -> LemonadeApp()
                         2 -> KocokDaduApp()
